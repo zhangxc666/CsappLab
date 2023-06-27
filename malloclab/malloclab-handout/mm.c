ef6df74e4f133262737fcf55b2711f48b8e48acb
zxc <1315730237@qq.com>
2023-06-28 01:07:17 +0800

    finish malloclab implicit link list

--- malloclab/malloclab-handout/mm.c
+++ malloclab/malloclab-handout/mm.c
@@ -1,6 +1,6 @@
-/*
+﻿/*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
+ *
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -32,40 +32,212 @@ team_t team = {
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
-    ""
-};
+    ""};
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
-
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
-
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-
-/* 
+#define WSIZE 4 // 单字长
+#define DSIZE 8 // 双字长
+#define MINSIZE 16
+#define CHUNKSIZE (1 << 12)                                         // 初始化堆时的大小和扩展堆的默认大小
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))                         // max操作
+#define PACK(size, alloc) ((size) | (alloc))                        // 将当前size和alloc值组合
+#define GET(x) (*(unsigned int *)(x))                               // 取出当前x地址的值
+#define PUT(x, val) (*(unsigned int *)(x) = (val))                  // 将x地址的值置为val
+#define GET_SIZE(x) ((GET(x)) & (~0x7))                             // 得到x地址块的大小
+#define GET_ALLOC(x) ((GET(x)) & 0x1)                               // 得到x地址块的分配位
+#define HDRP(bp) ((char *)(bp)-WSIZE)                               // 获取当前块的头部
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)        // 获取当前块的尾部
+#define NEXT_BLKP(bp) ((char *)bp + GET_SIZE(((char *)(bp)-WSIZE))) // 获取下一块的地址
+#define PREV_BLKP(bp) ((char *)bp - GET_SIZE(((char *)(bp)-DSIZE))) // 获取上一块的地址
+/*
  * mm_init - initialize the malloc package.
  */
+
+static void debugAllBlock(){
+    char *bp;
+    printf("\n");
+    printf("---------------开始debug-------------------\n");
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
+    {
+        
+        int alloc=GET_ALLOC(HDRP(bp)),size=GET_SIZE(HDRP(bp));
+        printf("--当前%p位置的空闲块：\n",bp);
+        printf("--大小为：\t%d\n",size);
+        printf("--分配状态：\t%s\n",(alloc)?"是":"否");
+        printf("\n");
+    }
+    printf("--结尾块地址：%p \n",bp);
+    printf("\n");
+    printf("---------------结束debug-------------------\n");
+    printf("\n");
+}
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE; // 地址按双字对齐，若奇数，字节再加1
+    if ((long)(bp = mem_sbrk(size)) == -1)
+    {
+        return NULL;
+    }
+    // printf("--打印操作：进行 extend_heap 操作，扩充 %d 大小的内存，地址为 %p \n",size,bp);
+    PUT(HDRP(bp), PACK(size, 0));         // 设置头部
+    PUT(FTRP(bp), PACK(size, 0));         // 设置尾部
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // 设置结尾块
+    bp=coalesce(bp); 
+    // debugAllBlock();
+    return bp;                  // 如果上一块是空闲块，合并上一块
+}
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(bp))); // 上一块是否分配
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp))); // 下一块是否分配
+    // printf("--打印操作：进行 coalesce 操作，地址为 %p 的上一块分配状态：%s ，下一块分配状态：%s\n",bp,(prev_alloc)?"是":"否",(next_alloc)?"是":"否");
+    size_t size = GET_SIZE(HDRP(bp));
+    if (prev_alloc && !next_alloc)
+    { // 下一块是空闲块
+        size_t x = (GET_SIZE(HDRP(NEXT_BLKP(bp)))) + size;
+        if(NEXT_BLKP(bp)==next_fit_bp)next_fit_bp=bp; // 当合并的块正好是下一次查询需要查询的块时
+        PUT(HDRP(bp), PACK(x, 0));
+        PUT(FTRP(bp), PACK(x, 0));
+    }
+    else if (!prev_alloc && next_alloc)
+    { // 上一块是空闲块
+        size_t x = (GET_SIZE(HDRP(PREV_BLKP(bp)))) + size;
+        PUT(HDRP(PREV_BLKP(bp)), PACK(x, 0)); // 先设置上一块的头部
+        PUT(FTRP(PREV_BLKP(bp)), PACK(x, 0)); // 设置当前块尾部
+        if(bp==next_fit_bp)next_fit_bp=PREV_BLKP(bp); // 当合并的块正好是下一次查询需要查询的块时
+        bp = PREV_BLKP(bp);                   // 修改bp为上一块的位置
+        
+    }
+    else if (!prev_alloc && !next_alloc)
+    { // 上下两块均是空闲块
+        size_t x = (GET_SIZE(HDRP(PREV_BLKP(bp)))) + size + (GET_SIZE(HDRP(NEXT_BLKP(bp))));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(x, 0)); // 设置上一块头部
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(x, 0)); // 设置下一块尾部
+        if(bp==next_fit_bp || NEXT_BLKP(bp)==next_fit_bp)next_fit_bp=PREV_BLKP(bp);// 当合并的块正好是下一次查询需要查询的块时
+        bp = PREV_BLKP(bp);                   // 修改bp为上一块的位置
+    }
+    return bp;
+}
+static void *find_fit(size_t new_size)// Perf index = 44 (util) + 23 (thru) = 67/100
+{
+    
+    void *bp;
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
+    {
+        if (GET_ALLOC(HDRP(bp)) == 0 && (new_size <= GET_SIZE(HDRP(bp))))
+        {
+            // printf("--打印操作：进行 find_fit 操作，找到大于等于 %d 大小的块，位置是在 %p \n",new_size,bp);
+            return bp;
+        }
+    }
+    // printf("--打印操作：进行 find_fit 操作，未找到大于 %d 大小的块\n",new_size);
+    return NULL;
+}
+static void *next_fit(size_t new_size){// Perf index = 44 (util) + 40 (thru) = 84/100
+    void *bp=next_fit_bp;
+    while(1){
+        if((GET_ALLOC(HDRP(bp))==0) && (new_size<= GET_SIZE(HDRP(bp)))){
+            next_fit_bp=bp;
+            return bp;
+        }
+        if(GET_SIZE(HDRP(bp))==0)bp=heap_listp;
+        else bp=NEXT_BLKP(bp);
+        if(bp==next_fit_bp)break;
+    }
+    return NULL;
+}
+static void *best_fit(size_t new_size){// Perf index = 45 (util) + 22 (thru) = 67/100
+    void *bp;
+    void *best_fit_bp=NULL;
+    for(bp=heap_listp;GET_SIZE(HDRP(bp))>0;bp=NEXT_BLKP(bp)){
+        if(GET_ALLOC(HDRP(bp))==0 && ((new_size<=(GET_SIZE(HDRP(bp)))) &&(best_fit_bp==NULL || GET_SIZE(HDRP(best_fit_bp))>=GET_SIZE(HDRP(bp))))){
+            best_fit_bp=bp;
+        }
+    }
+    return best_fit_bp;
+}
+static void place(void *bp, size_t new_size)
+{
+    // printf("--打印操作：进行 place 操作，当前 bp 的地址为 %p，空闲块大小为 %d ，分割大小为 %d \n",heap_listp,GET_SIZE(HDRP(bp)),new_size);
+    size_t dsize = GET_SIZE(HDRP(bp)) - new_size;
+    if (dsize < MINSIZE)
+    { // 当前剩余碎片小于MINSIZE
+        PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+        PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+    }
+    else
+    {
+        PUT(HDRP(bp), PACK(new_size, 1)); // 分配当前new_size
+        PUT(FTRP(bp), PACK(new_size, 1));
+        PUT(HDRP(NEXT_BLKP(bp)), PACK(dsize, 0)); // 割下剩下的块作为空闲块
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(dsize, 0));
+    }
+    // debugAllBlock();
+}
+static size_t getBlockSize(size_t size)
+{ // 获取实际的块的大小
+    size_t new_size;
+    if (size <= DSIZE)
+        new_size = DSIZE * 2; // 原因：当前首尾需要一个DSIZE，然后小于size需要对齐到dsize
+    else
+        new_size = DSIZE * (((size) + (DSIZE) + (DSIZE - 1)) / (DSIZE)); // payload+首尾开销，进行上取整
+    return new_size;
+}
+
 int mm_init(void)
 {
+    
+    if ((heap_listp = mem_sbrk(WSIZE << 2)) == (void *)-1)
+    { // 当前初始化失败
+        return -1;
+    }
+    // 初始化成功，当前堆占4个word（一个word==4Bytes）
+    PUT(heap_listp, 0);
+    PUT(heap_listp + WSIZE, PACK(DSIZE, 1));        // 设置序言头部大小
+    PUT(heap_listp + (WSIZE *2), PACK(DSIZE, 1)); // 设置序言尾部大小
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));      // 设置结尾
+    heap_listp += DSIZE;
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL) // 初始化默认空闲块大小
+        return -1;
+    next_fit_bp=heap_listp;
+    // printf("--打印操作：开始初始化 当前 heap_listp 的地址为 %p \n",heap_listp);
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
+
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    
+    // printf("--打印操作：进行 malloc 操作，分配 %d 字节的虚拟内存\n",size);
+    size_t new_size;
+    size_t extendsize;
+    char *bp;
+    if (!size)
+        return NULL;
+    new_size = getBlockSize(size);
+    if ((bp = best_fit(new_size)) != NULL)
+    {                        // 使用某种算法，找到一个满足要求的空闲块
+        place(bp, new_size); // 进行分割
+        return bp;
     }
+    extendsize = MAX(new_size, CHUNKSIZE); // 若当前未找到，扩展heap，向上开辟
+    if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+    { // 扩展失败
+        return NULL;
+    }
+    place(bp, new_size);
+    // debugAllBlock();
+    return bp;
 }
 
 /*
@@ -73,38 +245,38 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    // printf("--打印操作：进行 free 操作，释放 %p 位置的虚拟内存\n",ptr);
+    size_t size = GET_SIZE(HDRP(ptr));
+    PUT(HDRP(ptr), PACK(size, 0)); // 修改当前块首部的分配位
+    PUT(FTRP(ptr), PACK(size, 0)); // 修改当前块尾部的分配位
+    coalesce(ptr);                 // 合并
+    // debugAllBlock();
 }
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
-void *mm_realloc(void *ptr, size_t size)
+void *mm_realloc(void *ptr, size_t re_size)
 {
+    // printf("--打印操作：进行 realloc 操作，重新给 %p 位置的虚拟内存分配 %d 字节的虚拟内存\n",ptr,re_size);
     void *oldptr = ptr;
+    if(ptr==NULL){ // 当此时ptr为NULL等价于mm_malloc(size)
+        return mm_malloc(re_size);
+    }
+    if(re_size==0){ // 当此时size为0，等价于调用mm_free(ptr)
+        mm_free(ptr);
+        return NULL;
+    }
+    // 正常realloc
     void *newptr;
-    size_t copySize;
-    
-    newptr = mm_malloc(size);
+    size_t copySize = GET_SIZE(HDRP(ptr)) - DSIZE; // payload长度
+    newptr = mm_malloc(getBlockSize(re_size));
     if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+        return NULL;
+    if (re_size < copySize)
+        copySize = re_size;           // 当前容量减小
+    memcpy(newptr, oldptr, copySize); // 拷贝数据至新的块
+    mm_free(oldptr);                  // 释放旧的块
+    // debugAllBlock();
+    return newptr;                    // 返回新的块地址
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- malloclab/malloclab-handout/mm.h
+++ malloclab/malloclab-handout/mm.h
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
-extern int mm_init (void);
-extern void *mm_malloc (size_t size);
-extern void mm_free (void *ptr);
+extern int mm_init(void);
+extern void *mm_malloc(size_t size);
+extern void mm_free(void *ptr);
 extern void *mm_realloc(void *ptr, size_t size);
-
-
-/* 
- * Students work in teams of one or two.  Teams enter their team name, 
+static void *extend_heap(size_t);        // 给堆增加size个字节
+static void *coalesce(void *);           // 合并当前块的上下块
+static void *find_fit(size_t);           // 对隐式空闲链表进行首次适配搜索
+static void *next_fit(size_t);
+static void place(void *, size_t);       // 分割当前获取的空闲块
+static size_t getBlockSize(size_t size); // size是payload，返回实际开辟的块的大小
+static void debugAllBlock();      // 打印当前所有的块的情况
+/*
+ * Students work in teams of one or two.  Teams enter their team name,
  * personal names and login IDs in a struct of this
  * type in their bits.c file.
  */
-typedef struct {
+typedef struct
+{
     char *teamname; /* ID1+ID2 or ID1 */
     char *name1;    /* full name of first member */
     char *id1;      /* login ID of first member */
@@ -20,4 +26,5 @@ typedef struct {
 } team_t;
 
 extern team_t team;
-
+static char *heap_listp;
+static char *next_fit_bp;
